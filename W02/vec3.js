e212e617012134148661346fcec499699ad61d4d
Kmori0612 <65213447+Kmori0612@users.noreply.github.com>
2020-05-17 22:11:12 +0900

    Second commit

--- W02/vec3.js
+++ W02/vec3.js
@@ -1,1 +1,42 @@
-vec3
+class Vec3
+{
+  constructor(x,y,z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+
+  }
+    min(){
+     function compareFunc(a, b) {
+     return b - a;
+}
+
+    var num = [x,y,z];
+    num.sort(compareFunc);
+    return num[2] ;
+
+
+    }
+
+    max(){
+      function compareFunc(a, b) {
+  return b - a;
+}
+
+var num = [x,y,z];
+num.sort(compareFunc);
+
+      return num[0];
+    }
+
+    mid(){
+      function compareFunc(a, b) {
+  return b - a;
+}
+
+var num = [x,y,z];
+num.sort(compareFunc);
+      return num[1];
+}
+
+}
