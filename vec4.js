5717442008febaefcf9c744a3ed719d4c386eeb0
Kmori0612 <65213447+Kmori0612@users.noreply.github.com>
2020-05-19 17:17:24 +0900

    Third commit

---
+++ vec4.js
@@ -0,0 +1,55 @@
+class Vec4
+{
+  constructor(x,y,z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+
+  }
+	min()
+	{
+		var min = this.x
+		if
+		{
+                 min > this.y
+		min = this.y;
+		}
+		if
+		{
+		min > this.z
+                min = this.z;
+		}
+                return min;
+	}
+
+	 max()
+        {
+                var max = this.x;
+                if
+                {
+                 this.y > max
+                max = this.y;
+                }
+                if
+                {
+                 this.z > max
+                max = this.z;
+                }
+                return max;
+        }
+
+	 mid()
+        {
+                var mid = this.x;
+                if
+                {
+                 mid > this.y
+                mid = this.y;
+                }
+                if
+                {
+                mid < this.z
+                mid = this.z;
+                }
+                return min;
+        }
