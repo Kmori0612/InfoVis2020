e9429984b774fe621fc29297772999b00fc7611b
Kmori0612 <65213447+Kmori0612@users.noreply.github.com>
2020-05-22 11:46:12 +0900

    Update main.js

--- W04/main.js
+++ W04/main.js
@@ -1,12 +1,10 @@
-<html>
-    <head>
-	<title>W04: Example 01</title>
-    </head>
-    <body>
-	<script src="three.js"></script>
-	<script src="https://threejs.org/build/three.js"> </script>
-	<script>
-	 main();
-	</script>
-    </body>
+<html> 
+	<head>
+	<title>W04: Example 01</title> </head>
+		<body>
+	<script src="https://threejs.org/build/three.js"> 
+		</script>
+<script src="main.js"></script> 
+<script>main();</script>
+</body>
 </html>
