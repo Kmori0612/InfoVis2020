a567c58a4b0a641c70505f3e424b6a2dde111b57
Kmori0612 <65213447+Kmori0612@users.noreply.github.com>
2020-06-06 19:11:25 +0900

    Update w08_ex2.js

--- w08/w08_ex2.js
+++ w08/w08_ex2.js
@@ -25,4 +25,16 @@ function main()
     });
 
     screen.loop();
+    
+     // Create a color map
+    var cmap = [];
+    for ( var i = 0; i < 256; i++ )
+    {
+        var S = i / 255.0; // [0,1]
+        var R = Math.max( Math.cos( ( S - 1.0 ) * Math.PI ), 0.0 );
+        var G = Math.max( Math.cos( ( S - 0.5 ) * Math.PI ), 0.0 );
+        var B = Math.max( Math.cos( S * Math.PI ), 0.0 );
+        var color = new THREE.Color( R, G, B );
+        cmap.push( [ S, '0x' + color.getHexString() ] );
+    }
 }
