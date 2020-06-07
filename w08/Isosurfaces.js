472d7600dd0d38ad747a0ffc1dd9bee381ceb7da
Kmori0612 <65213447+Kmori0612@users.noreply.github.com>
2020-06-07 19:34:22 +0900

    Update Isosurfaces.js

--- w08/Isosurfaces.js
+++ w08/Isosurfaces.js
@@ -84,7 +84,7 @@ function Isosurfaces( volume, isovalue )
         'dimensions': { 'width': 0.15, 'height': 1.2 }
     } ) );
 
-    material.color = new THREE.Color( 1, 0, 1 );
+    material.color = new THREE.Color( "red" );
 
     return new THREE.Mesh( geometry, material );
 
