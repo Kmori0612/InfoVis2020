60afccf2a14833ab8f15817d1562772fb5d11455
Kmori0612 <65213447+Kmori0612@users.noreply.github.com>
2020-06-07 22:01:42 +0900

    Update Isosurfaces1.js

--- w08/Isosurfaces1.js
+++ w08/Isosurfaces1.js
@@ -89,9 +89,13 @@ function Isosurfaces( volume, isovalue )
 	
     }  
 
-    var C0 = new THREE.Color().setHex( cmap[isovalue][0] );
+    var C0 = new THREE.Color().setHex( cmap[isovalue][1] );
+    var C1 = new THREE.Color().setHex( cmap[isovalue][1] );
+    var C2 = new THREE.Color().setHex( cmap[isovalue][0] );
 
      material.color = new THREE.Color( C0 );
+     material.color = new THREE.Color( C1 ); 
+     material.color = new THREE.Color( C2 );
 
     return new THREE.Mesh( geometry, material );
 
