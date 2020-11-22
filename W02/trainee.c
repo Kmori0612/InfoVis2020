123bf5ed73038277f28f5908ca9524a3815ef9e3
Kmori0612 <65213447+Kmori0612@users.noreply.github.com>
2020-11-22 14:49:47 +0900

    Update trainee.c

--- W02/trainee.c
+++ W02/trainee.c
@@ -1,12 +1,17 @@
-#include <stdio.h>
 
+#include <stdio.h>
+  
 int main(void)
 {
-int a, b,c;
-  a = 3;
-  b = 5;
-  
-printf("c = %d/n", a+b);
-
-return 0;
+ 
+  int a, b;
+  printf("1つ目の整数: a = ");
+  scanf("%d", &a);
+  printf("2つ目の整数: b = ");
+  scanf("%d", &b);
+ 
+  printf("a + b = %d\n", a+b);
+  printf("a - b = %d\n", a-b);
+ 
+  return 0;
 }
