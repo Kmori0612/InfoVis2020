ec3993c080d35bad65695a8f3b3a93d504c7c6d4
Kmori0612 <65213447+Kmori0612@users.noreply.github.com>
2020-11-22 15:00:20 +0900

    Update trainee.c

--- W02/trainee.c
+++ W02/trainee.c
@@ -5,10 +5,9 @@ int main(void)
 {
  
   int a, b;
-  printf("1つ目の整数: a = ");
-  scanf("%d", &a);
-  printf("2つ目の整数: b = ");
-  scanf("%d", &b);
+  a = 3;
+  b = 5;
+
  
   printf("a + b = %d\n", a+b);
   printf("a - b = %d\n", a-b);
